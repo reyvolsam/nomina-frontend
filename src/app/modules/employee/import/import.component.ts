e86a54fe00ad0083fd81caffc152abe23ea71e73
Samuel Regino Placido <samuel.regino@gnp.com.mx>
2019-11-19 17:38:02 -0600

    Implementacion modulo de importacion de empleado

--- src/app/modules/employee/import/import.component.ts
+++ src/app/modules/employee/import/import.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { EmployeeService } from 'src/app/services/employee-services/employee.service';
 import Swal from 'sweetalert2';
+import { Company } from 'src/app/models/Company';
+import { SharedServices } from 'src/app/services/shared-services/shared-services.service';
 
 @Component({
   selector: 'app-import',
@@ -11,46 +13,78 @@ import Swal from 'sweetalert2';
 })
 export class ImportComponent implements OnInit {
 
-  importForm: FormGroup;  
+  importForm: FormGroup;
   formData = new FormData();
-
+  loader_data: Boolean = false
+  companies_list: Company[] = []
+  submitted: Boolean = false
 
   constructor(
-    private formBuilder: FormBuilder, 
+    private formBuilder: FormBuilder,
     private httpClient: HttpClient,
+    private sharedServices: SharedServices,
     private employeeService: EmployeeService) { }
 
   ngOnInit() {
     this.importForm = this.formBuilder.group({
-      employee_file: ['']
+      employee_file: ['', [Validators.required]],
+      company_id: [null, [Validators.required]],
     });
+
+    this.getCompanyCatalogFromUser()
+  }
+
+  get c(){ return this.importForm.controls }
+
+
+  getCompanyCatalogFromUser()
+  {
+    this.loader_data = true
+    this.sharedServices.getCompanyCatalogFromUser()
+    .subscribe(
+      res => {
+        console.log(res)
+        this.loader_data = false
+        this.companies_list = res.data
+        this.companies_list.unshift({id: null, name: 'Seleccione una empresa...', contact: '', rfc: '', telephone: ''})
+        if(this.companies_list.length == 0){
+          Swal.fire('¡Atención!', res.message, 'warning')
+        }
+      },
+      error => {
+        console.log(error.error.message)
+        this.loader_data = false
+        Swal.fire('¡Error!', error.error.message, 'warning')
+      })
   }
 
   onFileSelect(event) {
-    console.log('event', event)
     if (event.target.files.length > 0) {
-      console.log('length', event.target.files.length)
       const file = event.target.files[0];
-      console.log('file', file)
       this.importForm.get('employee_file').setValue(file);
     }
   }//
 
   onSubmit() {
-    
-    //console.log('importForm', this.importForm.get('employee_file').value)
-    this.formData.append('employee_file', this.importForm.get('employee_file').value);
-    console.log(this.formData.get('employee_file'))
 
-    this.employeeService.importEmployee(this.formData)
-    .subscribe(
-    res => {
-      console.log(res)
-    },
-    error => {
-      console.log(error.error.message)
-      Swal.fire('¡Error!', error.error.message, 'warning')
-    })
+    this.submitted = true
+  //if (this.importForm.invalid) {
+    //  return;
+    //} else {
+      //console.log('importForm', this.importForm.get('employee_file').value)
+      //this.formData.append('employee_file', this.importForm.get('employee_file').value);
+      //this.formData.append('company_id', this.importForm.get('company_id').value);
+
+      /*this.employeeService.importEmployee(this.formData)
+      .subscribe(
+      res => {
+        console.log(res)
+      },
+      error => {
+        console.log(error.error.message)
+        Swal.fire('¡Error!', error.error.message, 'warning')
+      })*/
+    //}
 
   }
 
