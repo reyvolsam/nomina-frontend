3cf03aebfd8b4cd7b54fc57d2096d53dec70b288
Samuel Regino Placido <samuel43_7@hotmail.com>
2019-12-17 23:16:00 -0600

    configuracion para productivo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://consumeloquemexicoproduce.mx/esifiscal/sistema/nomina-backend55/public/api'
+  baseUrl: 'http://esifiscal.com/nomina-backend55/public/api'
+  //baseUrl: 'http://consumeloquemexicoproduce.mx/esifiscal/sistema/nomina-backend55/public/api'
 };
