0c9289e48e225eeeccdd5cb16bdc06a2d48f9720
LUIS_HERRERA <luis.herrera.dev@gmail.com>
2021-05-12 16:32:59 -0500

    se agrega listado de empresas seleccionable en home(escritorio)

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from 'src/app/models/Company';
+import { CompanyService } from '../../../../services/company/company.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +13,32 @@ export class HomeComponent implements OnInit {
     { nombre: 'Crear Empleado', icono: 'person_add_alt_1', url: '/employee/create' },
 
   ]
-  constructor() { }
+
+  companyList: Company[] = [];
+  idSelected: Number = 0;
+
+  constructor(
+    private companyService: CompanyService
+  ) { }
 
   ngOnInit() {
+    this.getCompanies();
+  }
+
+  getCompanies(){
+    this.companyService.get().subscribe((res: any) => {
+      console.log(res);
+      if (res.data.length > 0) {
+        this.companyList = res.data
+      }
+    }, error => {
+      Swal.fire('Error', error.error.message, 'error');
+    })
+  }
+
+  selected(id: number){
+    console.log('empresa seleccionada: ', id);
+    this.idSelected = id;
   }
 
 }
